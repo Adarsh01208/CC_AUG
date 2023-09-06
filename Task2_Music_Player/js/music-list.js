1f041435f2c5d82a032388728adfb65d72b04dc4
Adarsh01208 <adarshsingh01209@gmail.com>
2023-09-06 19:09:23 +0530

    first commit

---
+++ Task2_Music_Player/js/music-list.js
@@ -0,0 +1,39 @@
+
+let allMusic = [
+  {
+    name: "Apna Bana Le - Bhediya",
+    artist: "Arjit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Tum Kya Mile – Rocky and Rani Kii Prem Kahaani",
+    artist: "Arjit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Chaleya - Jawan",
+    artist: "Arjit Singh",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Kesariya - Brahmastra Part I",
+    artist: "Arjit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Phir Aur Kya Chahiye - Zara Hatke Zara Bachke",
+    artist: "Arjit Singh,Sachin-Jigar",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Heeriye - Happy Hardy And Heer",
+    artist: "Arjit Singh",
+    img: "music-6",
+    src: "music-6"
+  }
+];
